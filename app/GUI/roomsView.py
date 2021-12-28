7846ab80dcb7c4c9db1a5eb5ae4e237fa1d771d5
Anghelache Vlad <anghelache.vlad.1@gmail.com>
2021-12-28 17:09:34 +0200

    updated roomsView

--- app/GUI/roomsView.py
+++ app/GUI/roomsView.py
@@ -1,40 +1,56 @@
 import tkinter as tk
 from tkinter import ttk
-
-
-
+from PIL import ImageTk, Image
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-
-
-
-
         self.geometry("840x700")
+        self.config(bg="#FAE3E3")
         self.title('Rooms')
 
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=3)
-        self.columnconfigure(2, weight=1)
+        self.canvas = tk.Canvas(bg="#FAE3E3")
+        self.canvas.pack(side='left', fill="both", expand=1)
+
+        self.scrollbar = ttk.Scrollbar(
+            self,
+            orient='vertical',
+            command=self.canvas.yview
+        )
+        self.scrollbar.pack(side='right', fill='y')
+
+        self.canvas.bind('<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+
+        self.second_frame = tk.Frame(self.canvas, bg="#FAE3E3")
+
+        self.second_frame.columnconfigure(0, weight=1)
+        self.second_frame.columnconfigure(1, weight=3)
+        self.second_frame.columnconfigure(2, weight=1)
+
+        self.canvas.create_window((50, 0), window=self.second_frame, anchor='nw')
+
+        self.canvas['yscrollcommand'] = self.scrollbar.set
+
         self.option_add("*Font", "\"Microsoft JhengHei UI\" 10")
-        self.titleLabel = tk.Label(text='My Rooms', font=("Microsoft JhengHei UI", 40, "bold")  )
+
+        self.titleLabel = tk.Label(self.second_frame, text='My Rooms', bg="#FAE3E3",
+                                   font=("Microsoft JhengHei UI", 40, 'bold'))
         self.titleLabel.grid(column=1, row=0, pady=(10, 10))
 
         self.rooms = []
         for i in range(2):
             self.rooms.append(None)
-            self.rooms[i] = RoomFrame(self, i+1)
+            self.rooms[i] = RoomFrame(self.second_frame, i + 1)
 
 
 class RoomFrame(ttk.Frame):
-    def __init__(self, container, row):
+    def __init__(self, container, index):
         super().__init__(container)
 
         s = ttk.Style()
-        s.configure('RoomFrame.TFrame', background='green')
+        s.configure('RoomFrame.TFrame', background='#CFA5B4')
         self.configure(style='RoomFrame.TFrame', width=900)
 
         self.columnconfigure(0, weight=1)
@@ -44,30 +60,54 @@ class RoomFrame(ttk.Frame):
 
         # numele camerei
 
-        self.title = ttk.Label(self, text='Livingroom', font=("Microsoft JhengHei UI", 20, "bold"))
-        self.title.grid(columnspan=2, row=0, padx=(20,20),pady=(0,20))
+        s.configure(style='Title.TLabel', background="#CFA5B4")
+        self.title = ttk.Label(self, text='Livingroom', style='Title.TLabel',
+                               font=("Microsoft JhengHei UI", 20, "bold"))
+        self.title.grid(columnspan=2, row=0, padx=(20, 20), pady=(0, 20))
 
         # lista surselor de lumina din camera
 
         self.lightsList = []
 
-        vars = []
+        self.vars = []
         for i in range(10):
-            var = tk.IntVar()
-            vars.append(var)
-            self.lightsList.append(tk.Checkbutton(self, text="Light " + str(i + 1), variable=var))
-            self.lightsList[i].grid(column=0, row=i+1, sticky='W')
+            var = tk.IntVar(value=0)
+            self.vars.append(var)
+            self.lightsList.append(tk.Checkbutton(self, text="Light " + str(i + 1), bg='#CFA5B4',activebackground="#c9708f", variable=var, onvalue=1, offvalue=0, command=self.printLightsState))
+            self.lightsList[i].grid(column=0, row=i + 1, sticky='W', pady=5)
 
         # butoane
 
-        self.switchAllLightsOnButton = ttk.Button(self, text='Switch all lights on')
-        self.switchAllLightsOnButton.grid(column=3, row=1, padx=(100,0))
+        self.photoOn = ImageTk.PhotoImage(Image.open(r"Assets/LightOn.png").resize((20, 20)))
 
-        self.switchAllLightsOffButton = ttk.Button(self, text='Switch all lights off')
-        self.switchAllLightsOffButton.grid(column=3, row=2, padx=(100,0))
+        self.switchAllLightsOnButton = tk.Button(self, text='All lights on', background="#f9dd52", image=self.photoOn,relief='flat',
+                                                 activebackground="#fcd200", compound="left", command=self.switchOnAllLights)
+        self.switchAllLightsOnButton.grid(column=3, row=1, padx=(100, 0), ipadx=5, ipady=5)
 
-        self.grid(column=1, row=row, padx=20, pady=20)
+        self.photoOff = ImageTk.PhotoImage(Image.open(r"Assets/LightOff.png").resize((20, 20)))
+
+        self.switchAllLightsOffButton = tk.Button(self, text='All lights off', background="#5972a0",relief='flat',
+                                                  activebackground="#2c57a5",image=self.photoOff, compound="left", command=self.switchOffAllLights)
+        self.switchAllLightsOffButton.grid(column=3, row=2, padx=(100, 0),pady=(5,0), ipadx=5, ipady=5)
+
+        self.grid(column=1, row=index, padx=20, pady=20)
         self['padding'] = (100, 50)
+
+    def switchOffAllLights(self):
+        for var in self.vars:
+            var.set(0)
+
+    def switchOnAllLights(self):
+        for var in self.vars:
+            var.set(1)
+
+    def printLightsState(self):
+        for i in range(len(self.vars)):
+            if self.vars[i].get() == 0:
+                print(f'Light {i+1} is off')
+            else:
+                print(f'Light {i+1} is on')
+        print('\n')
 
 
 if __name__ == "__main__":
