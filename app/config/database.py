40f7283a1a51afca19df862a24219c5a77b24ac9
Taving40 <taving40@gmail.com>
2022-02-01 21:54:38 +0200

    Added AsyncAPI docs for wakeup/sleep functionality

--- app/config/database.py
+++ app/config/database.py
@@ -11,14 +11,14 @@ def create_db(app, db):
         db.create_all()
 
 def populate_db(app, db):
-    profile1, profile2 = models.UserProfile(profile_name="profile1"), models.UserProfile(profile_name="profile2")
+    profile1, profile2 = models.UserProfile(profile_name="profile1", is_active=True), models.UserProfile(profile_name="profile2", is_active=False)
     room1, room2 = models.Room(name="bedroom"), models.Room(name="kitchen")
     light1, light2 = models.Light(), models.Light()
     room1.lights = [light1, light2]
     profile1.rooms = [room1, room2]
     with app.app_context():
-        # db.session.add(profile1)
-        # db.session.add(profile2)
+        db.session.add(profile1)
+        db.session.add(profile2)
         db.session.commit()
 
 if __name__ == "__main__":
